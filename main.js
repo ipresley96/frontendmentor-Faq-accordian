78cb7efed9d81b538144dab96c2f0e1e828c2bd5
Isaac Presley <76850372+ipresley96@users.noreply.github.com>
2022-06-16 14:44:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const question = document.querySelectorAll('.accordian-header')
+
+question.forEach(item => item.addEventListener('click', () => {
+    item.classList.toggle('active')
+    // Add styling to children elements
+    const bold = item.firstElementChild;
+    const arrowTurn = item.lastElementChild;
+    if(item.classList.contains('active')){
+        bold.style.fontWeight = 'bold';
+        arrowTurn.style.transform = 'rotate(180deg)'
+    } else {
+        bold.style.fontWeight = 'normal';
+        arrowTurn.style.transform = 'rotate(0)'
+    }
+
+    // Display text 
+    const content = item.nextElementSibling
+    if(item.classList.contains('active')){
+        content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+        content.style.maxHeight = 0
+    }
+    })
+)
